1324c6d0b84af45f31e59bfe001754c93568e515
aditibongu <57454988+aditibongu@users.noreply.github.com>
2020-04-17 23:36:16 -0700

    Added all files from repl.it portfolio

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php echo '<p> Hello World! Here are all of my Links: Aditi Bongu, Period 1</p>'; ?> 
+    <ul>
+      <a href="index.html">Home Page</a>
+      <!-- Please make sure to change all the #'s to the proper link names-->
+      <a href="Bongu_AboutMe.html">About Me</a>
+      <a href="Bongu_Projects.html">Projects</a>
+      <a href="Bongu_Resume.html">Resume</a>
+      <a href="Bongu_ContactMe.html">Contact Me</a>
+      <!--git add . git commit -m "update" git push -u origin master to add/update github-->
+    <ul>
+  </body>
+</html>
